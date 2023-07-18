da11aa17206aff493c7da4e2a7afc9360a6db40c
caimi <caimi@meituan.com>
2023-07-18 10:01:08 +0800

    Hello World!

--- Algorithm.java
+++ Algorithm.java
@@ -1,7 +1,9 @@
 /**
- * @author caimi
+ * @author hy
  * @date 2023/5/4 10:57
  */
 public class Algorithm {
-
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# caimiStudy
+# hyStudy
